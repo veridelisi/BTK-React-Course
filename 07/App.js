b11643ce145a63333fd3f2616af095db88eda482
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-01-07 16:10:31 +0300

    Create App.js

---
+++ 07/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react'
+import Navi from './Navi'
+import CategoryList from './CategoryList'
+import ProductList from './ProductList'
+import { Container, Row, Col } from 'react-bootstrap'
+
+export default class App extends Component {
+
+  state = { currentCategory: "" }
+
+  
+  changeCategory = category => {
+
+    this.setState({ currentCategory: category.categoryName })
+  }
+
+  render() {
+    let productInfo = { title: "Product List" }
+    let categoryInfo = { title: "Category List" }
+    return (
+      <div>
+        <Container>
+          <Row>
+
+            <Navi></Navi>
+          </Row>
+
+          <Row>
+           
+            <Col xs="3">
+             
+              <CategoryList
+                currentCategory={this.state.currentCategory}
+                changeCategory={this.changeCategory}
+                info={categoryInfo}>
+              </CategoryList>
+            </Col>
+
+
+          <Col xs="9">
+              <ProductList 
+              currentCategory={this.state.currentCategory}
+              info={productInfo}>
+              </ProductList></Col>
+
+          </Row>
+
+        </Container>
+
+      </div>
+    )
+  }
+}
