e2d3ea3aadcc401d0a0d7df181f45434d3ca3610
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-01-07 10:30:43 +0300

    Create CategoryList.js

---
+++ 02/CategoryList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class CategoryList extends Component {
+    render() {
+        return (
+            <div>
+                <h2>This text from CategoryList.js</h2>
+            </div>
+        )
+    }
+}
