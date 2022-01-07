3517b6da19b91d215f324dc83c048d72c188b420
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-01-07 10:23:22 +0300

    Create Navi.js

---
+++ 01/Navi.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export default class Navi extends Component {
+    render() {
+        return (
+            <div>
+                <h2>This text from Navi.js</h2>
+                
+            </div>
+        )
+    }
+}
