503c00273246fedead9e633c0cf3bcfdfca46bd4
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-01-07 16:02:10 +0300

    Create App.js

---
+++ 05/App.js
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+import Navi from './Navi'
+import CategoryList from './CategoryList'
+import ProductList from './ProductList'
+import { Container, Row, Col } from 'react-bootstrap'
+
+export default class App extends Component {
+  
+  render() {
+
+    let productInfo={title:"Product List"}
+    let categoryInfo={title:"Category List"}
+    return (
+      <div>
+        <Container>
+          <Row>
+
+          <Navi></Navi>
+          </Row>
+
+          <Row>
+        
+            <Col xs="3" ><CategoryList info={categoryInfo}></CategoryList></Col>
+            <Col xs="9"><ProductList  info={productInfo}></ProductList></Col>
+
+          </Row>
+
+        </Container>
+
+      </div>
+    )
+  }
+}
