6166dee290100958ced26965131cd0b76e1cc369
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-01-07 10:22:26 +0300

    Create App.js

---
+++ 01/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Navi from'./Navi'
+
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <Navi></Navi>
+        
+      </div>
+    )
+  }
+}
