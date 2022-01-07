f803d2b01110d20e4dba98578541fb3c61931f1b
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-01-07 18:54:03 +0300

    Create CategoryList.js

---
+++ 08/CategoryList.js
@@ -0,0 +1,50 @@
+import React, { Component } from 'react'
+import { ListGroup, ListGroupItem } from 'react-bootstrap'
+
+
+export default class CategoryList extends Component {
+
+    /* CategoryList DataBase*/
+    state = {
+        categories: []
+            }
+
+    componentDidMount(){
+        this.getCategories()
+    }
+
+    getCategories = () => {
+        fetch("http://localhost:3000/categories")
+            .then(response => response.json())
+            .then(response => this.setState({ categories: response }))
+    }
+
+
+
+    render() {
+        return (
+            <div>
+                <h2>{this.props.info.title}</h2>
+                <h3>{this.state.counter}</h3>
+                <ListGroup>
+                
+                    {
+                        //CategoryList Items with Map
+                        this.state.categories.map(category => (
+                            <ListGroupItem
+                                key={category.id}
+                                onClick={() => this.props.changeCategory(category)}>
+
+                                {category.categoryName}
+
+                            </ListGroupItem>
+                        ))
+                    }
+
+                </ListGroup>
+                
+                <h4>{this.props.currentCategory}</h4>
+            </div>
+        )
+    }
+}
