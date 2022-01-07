76206f71fb0061894ea1e53cee26f865e23e99d0
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-01-07 22:24:47 +0300

    Add files via upload

---
+++ 06/Navi.js
@@ -0,0 +1,13 @@
+
+import React, { Component } from 'react'
+
+export default class Navi extends Component {
+    render() {
+        return (
+            <div>
+                <h2>This text from Navi.js</h2>
+                
+            </div>
+        )
+    }
+}
