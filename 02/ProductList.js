71504b403e6ab09e3081277bf2ea37fe9ff1bf32
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-01-07 10:31:09 +0300

    Create ProductList.js

---
+++ 02/ProductList.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class ProductList extends Component {
+    render() {
+        return (
+            <div>
+                <h2>This text from ProductList.js</h2>
+            </div>
+        )
+    }
+}
