5b0857a18e2ecb7b4ea2b6adec027fb7ced91885
veridelisi <72710290+veridelisi@users.noreply.github.com>
2022-01-09 22:52:32 +0300

    Create FormDemo1.js

---
+++ 20/FormDemo1.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+
+export default class FormDemo1 extends Component {
+    state={userName:""}
+    onChangeHandler=(event) =>{
+        this.setState({userName: event.target.value})
+    }
+    render() {
+        return (
+            <div>
+                <form>
+
+                    <h3>UserName</h3>
+                    <input   onChange={this.onChangeHandler}     type="text"></input>
+                    <h3>User Name is {this.state.userName}</h3>
+                    
+                </form>
+                
+            </div>
+        )
+    }
+}
